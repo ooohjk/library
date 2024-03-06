a56156a8163dc7fd1c18764be618f2455e3edfc2
shson <sunghyun7895@naver.com>
2024-03-06 17:06:24 +0900

    OAuth2 로그인 개발 v2.0 - 소셜로그인타입 enum 클래스 정리 및 테스트 검증

---
+++ src/main/java/com/example/library/user/enumPk/SocialLoginType.java
@@ -0,0 +1,18 @@
+package com.example.library.user.enumPk;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+public enum SocialLoginType {
+    GOOGLE,
+    NAVER,
+    KAKAO;
+
+    public static SocialLoginType getSocialType(String type){
+        return Arrays.stream(SocialLoginType.values())
+                .filter(socialLoginType->socialLoginType.name().equals(type.toUpperCase()))
+                .findFirst()
+                .orElseThrow(()->new RuntimeException("존재하지 않는 소셜로그인 타입입니다."))
+        ;
+    }
+}
---
+++ src/test/java/com/example/library/user/enumPk/SocialLoginTypeTest.java
@@ -0,0 +1,21 @@
+package com.example.library.user.enumPk;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SocialLoginTypeTest {
+
+    @Test
+    public void 문자형의소셜로그인을Eum클래스로변환테스트(){
+        String lowerType = "Google";
+        String UpperType = "GOOGLE";
+        SocialLoginType SocialLoginTypeFromLower = SocialLoginType.getSocialType(lowerType);
+        SocialLoginType SocialLoginTypeFromUpper = SocialLoginType.getSocialType(UpperType);
+
+        Assertions.assertThat(SocialLoginTypeFromLower).isEqualTo(SocialLoginType.GOOGLE);
+        Assertions.assertThat(SocialLoginTypeFromUpper).isEqualTo(SocialLoginType.GOOGLE);
+
+    }
+}
