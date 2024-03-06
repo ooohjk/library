7ff82ece6f1e9ad288b0fa50cab8a187319d7b78
ooohjk <ojk951204@gmail.com>
2024-03-06 13:21:21 +0900

    user 일반 회원가입 및 로그인

--- src/main/java/com/example/library/user/controller/UserController.java
+++ src/main/java/com/example/library/user/controller/UserController.java
@@ -3,8 +3,6 @@ package com.example.library.user.controller;
 import com.example.library.user.dto.UserDto;
 import com.example.library.user.service.UserService;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/user")
 @Tag(name = "user")
 public class UserController {
-    private final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
     private final UserService userService;
 
     @Autowired
@@ -26,12 +23,6 @@ public class UserController {
         userService.join(userDto.getUserId(), userDto.getUserPwd(), userDto.getUserName(), userDto.getTel(), userDto.getEmail(), userDto.getGender(), userDto.getUseFlg());
         return ResponseEntity.ok().body("Success Join!!");
     }
-
-//    @PostMapping("/login")
-//    public ResponseEntity<String> login(@RequestBody UserDto userDto) {
-//        String token = userService.login(userDto.getUserId(), userDto.getUserPwd());
-//        return ResponseEntity.ok().body(token);
-//    }
 
     @PostMapping("/login")
     public ResponseEntity<String> login(String userId, String userPwd) {
