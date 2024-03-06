a02e89765787430917bae496102efc70fc82b41c
shson <sunghyun7895@naver.com>
2024-03-06 19:38:50 +0900

    OAuth2 로그인 개발 v2.2 - oauth2 인증 필터 커스터마이징 및 시큐리티 설정 추가

--- src/main/java/com/example/library/config/AuthenticationConfig.java
+++ src/main/java/com/example/library/config/AuthenticationConfig.java
@@ -10,11 +10,14 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableWebSecurity(debug = true)
+@EnableWebSecurity
 @RequiredArgsConstructor
 public class AuthenticationConfig {
 
@@ -42,8 +45,16 @@ public class AuthenticationConfig {
                     session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
                 })
                 //2024.03.06 sunghyun oauth2 설정
+                /*
+                 * 우리가 서비스를 만들 때 회원가입이라는 절차를 통해 사용자를 등록하고 관리하게 됩니다. 이때 사용자가 등록한 정보가 유효한 정보인지 인증(Authentication)하는 과정은 결코 쉽지 않고 복잡한 인증 프로세스를 요구하게 됩니다.
+                 * 이러한 문제를 해결하기 위해 우리는 사용자가 이미 가입한 거대 플랫폼 기업의 인증된 사용자 정보에 접근하여 정보를 얻어올 수 있습니다.
+                 * 이때 사용하는 프로토콜이 바로 OAuth(Open Authrization)입니다.
+                 * OAuth의 핵심은 사용자 정보를 가지고 있는 서버(구글, 카카오...)로부터 사용자 정보에 대한 접근 권한을 인가(Authorization) 받아 정보를 가져오는 것입니다.
+                 * 여기서 인가(Authorization)란 사용자에게 특정 리소스나 기능에 액세스 할 수 있는 권한을 부여하는 프로세스를 말합니다.
+                 */
                 .oauth2Login(oauth2->oauth2
-                        .authorizationEndpoint(authorizationEndpointConfig -> authorizationEndpointConfig.baseUri("/api/user/oauth2/authorize"))) //인증을 위한 url이며 default url은 /oauth2/authorization/{registration}, 옆과 같이 수정 시 /api/user/oauth2/authorize/{registration}으로 접근
+                        .authorizationEndpoint(authorizationEndpointConfig -> authorizationEndpointConfig.baseUri("/api/user/oauth2/authorize")) //인증을 위한 url이며 default url은 /oauth2/authorization/{registration}, 옆과 같이 수정 시 /api/user/oauth2/authorize/{registration}으로 접근가능
+                        .userInfoEndpoint(userInfoEndpoint->userInfoEndpoint.userService((OAuth2UserService<OAuth2UserRequest, OAuth2User>) userService))) //인가된 정보를 활용하여 당사에서 활용할 수 있도록 커스터마이징
                 .addFilterBefore(new JwtFilter(userService, secret), UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
--- src/main/java/com/example/library/user/service/Impl/UserServiceImpl.java
+++ src/main/java/com/example/library/user/service/Impl/UserServiceImpl.java
@@ -4,28 +4,32 @@ import com.example.library.exception.AppException;
 import com.example.library.exception.ErrorCode;
 import com.example.library.user.entity.UserEntity;
 import com.example.library.user.dto.UserDto;
+import com.example.library.user.enumPk.SocialLoginType;
 import com.example.library.user.repository.UserRepository;
 import com.example.library.user.service.UserService;
 import com.example.library.utils.JwtUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
-public class UserServiceImpl implements UserService {
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService , OAuth2UserService<OAuth2UserRequest, OAuth2User> {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder encoder;
     @Value("${jwt.secret}")
     private String secret;
     private final Long expiredMs = 1000 * 60 * 60L;
-
-    @Autowired
-    public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder encoder) {
-        this.userRepository = userRepository;
-        this.encoder = encoder;
-    }
 
     public String join(String userId, String userPwd, String userName, String tel, String email, String gender, Integer userFlg) {
         userRepository.findByUserId(userId).ifPresent(user -> {
@@ -72,5 +76,32 @@ public class UserServiceImpl implements UserService {
 
         UserDto userDto = new UserDto(userEntity.getUserId(), userEntity.getUserPwd(), userEntity.getUserName(), userEntity.getTel(), userEntity.getUserEmail(), userEntity.getGender(), userEntity.getUseFlg());
         return userDto;
+    }
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        /*
+         * DefaultOAut2UserService 객체 생성 후 해당 객체를 통하여 loadUser메소드를 통해 OAuth2User 반환
+         * DefaultOAut2UserService.loadUser()메소드는 역할은 간략히 아래 순서로 흐른다
+         * 1. 소셜로그인 API의 사용자 정보를 위한 통신
+         * 2. 소셜로그인 API로부터 응답 받은 사용자 정보로 DefaultOAuth2User 객체 생성 후 반환
+         * 따라서!! OAuth2User객체는 사용자 정보가 담긴 객체!
+         */
+        OAuth2UserService<OAuth2UserRequest,OAuth2User> delegate = new DefaultOAuth2UserService();
+        OAuth2User oAuth2User = delegate.loadUser(userRequest);
+
+        //1. 당사에 가입된 유저 확인을 데이터 추출
+        SocialLoginType socialLoginType= SocialLoginType.getSocialType(userRequest.getClientRegistration().getRegistrationId());
+        String email = getDataFromOAuth2User(oAuth2User,"email");
+        String name = getDataFromOAuth2User(oAuth2User,"name");
+        String providerId = oAuth2User.getAttribute(userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName());
+
+        //2. 당사 존재 여부 파악 (조회조건: 소셜플랫폼 종류, email, socialId )
+
+        return null;
+    }
+
+    private String getDataFromOAuth2User(OAuth2User oAuth2User,String type){
+        return (String)oAuth2User.getAttributes().get(type);
     }
 }
