016b243c66c8097cf9a29d736d40020ab7a8863d
shson <sunghyun7895@naver.com>
2024-03-14 15:07:42 +0900

    도서 찜 개발 v1.0 - 찜 관련 Exception 및 에러코드 추가

--- src/main/java/com/example/library/exception/ErrorCode.java
+++ src/main/java/com/example/library/exception/ErrorCode.java
@@ -10,21 +10,25 @@ public enum ErrorCode {
     SUC("0","OK"),
 
     //User
-    USERID_DUPLICATED("1", "중복 아이디입니다. 아이디를 확인해주세요."),
-    USERID_NOT_FOUND("2", "존재하지 않는 아이디입니다. 아이디를 확인해주세요."),
-    USERNO_NOT_FOUND("3", "존재하지 않는 유저번호입니다. 번호를 확인해주세요."),
-    PASSWORD_DIFFERNET("4", "올바르지 않은 패스워드입니다. 패스워드를 확인해주세요."),
+    USERID_DUPLICATED("U01", "중복 아이디입니다. 아이디를 확인해주세요."),
+    USERID_NOT_FOUND("U02", "존재하지 않는 아이디입니다. 아이디를 확인해주세요."),
+    PASSWORD_DIFFERNET("U03", "올바르지 않은 패스워드입니다. 패스워드를 확인해주세요."),
+    USERNO_NOT_FOUND("U04", "존재하지 않는 유저번호입니다. 유저번호를 확인해주세요."),
 
+    //Heart
+    HEARTBOOK_EMPTY("H01","찜한 도서가 없습니다."),
+    HEARTNO_NOT_FOUND("H02","존재하지 않는 찜번호입니다. 찜번호를 확인해주세요."),
+    HEARTBOOK_ALREADY("H3","이미 찜한 도서입니다."),
     //Book
-    BOOKNAME_NOT_FOUND("5","존재하지 않는 책이름입니다. 다시 확인해주세요."),
-    BOOKAUTHOR_NOT_FOUND("6","존재하지 않는 저자입니다. 다시 확인해주세요."),
-    BOOKCODE_NOT_FOUND("7","존재하지 않는 책번호입니다. 다시 확인해주세요."),
+    BOOKNAME_NOT_FOUND("B01","존재하지 않는 책이름입니다. 다시 확인해주세요."),
+    BOOKAUTHOR_NOT_FOUND("B02","존재하지 않는 저자입니다. 다시 확인해주세요."),
+    BOOKCODE_NOT_FOUND("B03","존재하지 않는 책번호입니다. 다시 확인해주세요."),
 
     //Mail
-    MAIL_NOT_FOUND("8", "존재하지 않는 메일주소입니다. 메일주소를 확인해주세요."),
+    MAIL_NOT_FOUND("M01", "존재하지 않는 메일주소입니다. 메일주소를 확인해주세요."),
 
     //Review
-    REVIEW_NOT_FOUND("9", "해당도서의 리뷰가 없습니다.")
+    REVIEW_NOT_FOUND("R01", "해당도서의 리뷰가 없습니다.")
 
     ;
 
