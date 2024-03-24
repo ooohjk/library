179b732f2fbc745a9a204af9a8b908ce7889e7ca
shson <sunghyun7895@naver.com>
2024-03-25 02:22:56 +0900

    cors 설정 등록

---
+++ src/main/java/com/example/library/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.example.library.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
+                .maxAge(3600L)
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization")
+                .allowCredentials(true);
+        ;
+    }
+
+
+}
